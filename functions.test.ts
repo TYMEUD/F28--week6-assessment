7d1e079a66f3b50a6aa830920afaf2c32638b342
Ty Menlove <tjmundecided@gmail.com>
2022-09-23 16:39:29 -0600

    added tests

--- functions.test.ts
+++ functions.test.ts
@@ -2,4 +2,14 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    test('check to see if the length is the same', () => {
+        let arr = [1,2,3,4,5]
+        let result = shuffleArray(arr)
+        expect(arr.length).toBe(result.length)
+    })
+
+    test('Check to see if shuffleArray returns array', () => {
+        let arr = [1,2,3,4,5]
+        expect(shuffleArray(arr)).toBe(true)
+    })
 })
